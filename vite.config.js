f161abf8389bcd3fac900a413df13dd753387c14
krishna kishore <kishoretvk@hotmail.com>
2025-07-29 00:18:44 -0500

    updated

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/RawImageEditor/', // Set the base path to match your GitHub Pages repository
+})
